7353284cce2df4459b96ef5aff94f472c9f6dbe4
Kevin F. CALMO <kevinfcalmo@gmail.com>
2023-09-04 15:00:53 -0400

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
