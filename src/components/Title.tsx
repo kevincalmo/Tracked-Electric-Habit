62f6fda41435297e4f7c5281917344902c42abbe
Kevin F. CALMO <kevinfcalmo@gmail.com>
2023-09-04 16:27:43 -0400

    Création du composant titre

---
+++ src/components/Title.tsx
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react";
+
+const Title = () => {
+  return <Heading 
+  as="h2"
+  textAlign="center"
+  textTransform="uppercase"
+  >Tracked Electric Habit</Heading>;
+};
+
+export default Title;
